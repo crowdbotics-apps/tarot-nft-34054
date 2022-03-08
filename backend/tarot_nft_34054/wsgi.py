6527781567191d133c31313515a16ca950c382f1
Crowdbotics <team@crowdbotics.com>
2022-03-08 16:45:54 +0000

    Initial commit.

---
+++ backend/tarot_nft_34054/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tarot_nft_34054 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tarot_nft_34054.settings')
+
+application = get_wsgi_application()
